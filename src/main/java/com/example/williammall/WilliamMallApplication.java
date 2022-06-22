0c97fd4a0251dcecbe24cfaeace20ce0eac076df
charliezong <charliezong18@gmail.com>
2022-06-22 18:36:55 -0400

    initial push

---
+++ src/main/java/com/example/williammall/WilliamMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.williammall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilliamMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WilliamMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/williammall/WilliamMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.williammall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliamMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
